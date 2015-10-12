50a29f4ce0695069a2016334d8c1d39ccf7cecc8
NalindaHemanga <nalinda2hemanga@gmail.com>
2015-10-12 13:34:24 +0530

    test

--- item_issue_panel2.php
+++ item_issue_panel2.php
@@ -11,6 +11,22 @@
 <link rel="stylesheet" type="text/css" href="css/dashboardicon.css" />
 <link rel="stylesheet" type="text/css" href="css/itemissue.css" />
 <script src="lib/jquery.min.js"></script>
+<script type="text/javascript">
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+
+}
+</script>
 
 
 
@@ -31,17 +47,29 @@
 
         	</div>
         <div id="content">
+            <form>
             <div class="" id="leftpane">
                 <div style="position:relative;top:50%;text-align:center;">No Items to be shown !!</div>
             </div>
 
-            <div class="" id="rightpane">
-                
+            <div class="" id="rightpane" ondrop="drop(event)" ondragover="allowDrop(event)">
+
                     <div style="text-align:center;">Drag and Drop Item copies here to issue</div>
             </div>
 
+            <div class="" id="bottomleft">
+
+            </div>
+
+            <div id="bottomright">
+                <div class="" style="text-align:center">
+                    <img src="img/icons/trash.png" width="100" height="100" alt="" />
+                </div>
+
+            </div>
 
 
+        </form>
         </div>
         </div>
 
--- test.php
+++ test.php
@@ -58,14 +58,10 @@ function drag(ev) {
 }
 
 function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-   var nodeCopy = document.getElementById(data).cloneNode(true);
-    nodeCopy.id = "newId"; /* We cannot use the same ID */
-var option = document.createElement("option");
-var node = document.createTextNode("This is new.");
-option.appendChild(node)
-    ev.target.appendChild(option);
+    
+	ev.preventDefault();
+   var data = ev.dataTransfer.getData("text");
+   ev.target.appendChild(document.getElementById(data));
 }
 </script>
 </head>
@@ -80,15 +76,3 @@ option.appendChild(node)
 
 </body>
 </html>
-
-
-						
-        				
-        			
-
-
-
-
-
-
-

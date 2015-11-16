85b1d661d388c808ea444f0e75889742ad661f04
NalindaHemanga <nalinda2hemanga@gmail.com>
2015-11-16 12:33:26 +0530

    added index file

--- item_return_panel2.php
+++ item_return_panel2.php
@@ -10,6 +10,81 @@
 <link rel="stylesheet" type="text/css" href="css/wrapper.css" />
 <link rel="stylesheet" type="text/css" href="css/form.css" />
 <link rel="stylesheet" type="text/css" href="css/forumtable.css" />
+
+<style>
+[type="checkbox"]:not(:checked),
+[type="checkbox"]:checked {
+  position: absolute;
+  left: -9999px;
+}
+[type="checkbox"]:not(:checked) + label,
+[type="checkbox"]:checked + label {
+  position: relative;
+  padding-left: 75px;
+  cursor: pointer;
+}
+[type="checkbox"]:not(:checked) + label:before,
+[type="checkbox"]:checked + label:before,
+[type="checkbox"]:not(:checked) + label:after,
+[type="checkbox"]:checked + label:after {
+  content: '';
+  position: absolute;
+}
+[type="checkbox"]:not(:checked) + label:before,
+[type="checkbox"]:checked + label:before {
+  left:0; top: -3px;
+  width: 130px; height: 20px;
+  background: #DDDDDD;
+  border-radius: 15px;
+  transition: background-color .2s;
+}
+[type="checkbox"]:not(:checked) + label:after,
+[type="checkbox"]:checked + label:after {
+  width: 10px; height: 10px;
+  transition: all .2s;
+  border-radius: 50%;
+  background: #7F8C9A;
+  top: 2px; left: 5px;
+}
+
+/* on checked */
+[type="checkbox"]:checked + label:before {
+  background:#34495E; 
+}
+[type="checkbox"]:checked + label:after {
+  background: #39D2B4;
+  top: 2px; left: 115px;
+}
+
+[type="checkbox"]:checked + label .ui,
+[type="checkbox"]:not(:checked) + label .ui:before,
+[type="checkbox"]:checked + label .ui:after {
+  position: absolute;
+  left: 6px;
+  width: 100px;
+  border-radius: 10px;
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 16px;
+  transition: all .2s;
+}
+[type="checkbox"]:not(:checked) + label .ui:before {
+  content: "Not Returned";
+  left: 32px
+}
+[type="checkbox"]:checked + label .ui:after {
+  content: "Returned";
+  color: #39D2B4;
+}
+[type="checkbox"]:focus + label:before {
+  border: 1px dashed #777;
+  box-sizing: border-box;
+  margin-top: -1px;
+}
+
+
+
+</style>
 </head>
 <body>
     <div id="wrapper">
@@ -36,7 +111,8 @@
 						$desc=$value->getPurpose();
 						$bdate=$value->getBorrowedDate();
 						$edate=$value->getExpectedReturnDate();
-						$comment=$value->getBorrowComment();
+						$bcomment=$value->getBorrowComment();
+						$rcomment=$value->getReturnComment();
 
 
 						
@@ -48,7 +124,7 @@
 			<tr><th>Transaction Description</th><td><?php echo $desc; ?></td></tr>
 			<tr><th>Borrowed Date</th><td><?php echo $bdate; ?></td></tr>
 			<tr><th>Expected Return Date</th><td><?php echo $edate; ?></td></tr>
-			<tr><th>Comments</th><td><?php echo $comment; ?></td></tr>
+			<tr><th>Comments</th><td><?php echo $rcomment; ?></td></tr>
 
 
 			</table>
@@ -61,7 +137,7 @@
         	<?php
 
         			echo "<table>";
-					echo "<thead><th>Item Copy No</th><th>Borrowed Quantity</th><th>Return Quantity</th><th>Condition</th><th>Return</th></thead><tbody>";
+					echo "<thead><th>Item Copy No</th><th>Borrowed Quantity</th><th>Return Quantity</th><th>Condition</th><th>Return Status</th></thead><tbody>";
 
 
 					$result=$value->getTransactions();
@@ -71,6 +147,7 @@
 						$itemCopyNo = $itemCopy->get_no();
 						$bcon=$value->getBorrowedQuantity();
 						$rcon=$value->getReturnedQuantity();
+					
 						
 
 
@@ -79,13 +156,13 @@
 						echo "<td><input type='number' class='large text' value='$bcon'/></td>";
 						echo "<td>";
 						echo "<select class='large select' name='condition'>";
-						echo "<option value='Working Properly'>Working Properly</option>";
+						echo "<option value='Working Properly' selected='selected'>Working Properly</option>";
 						echo "<option value='Working With defects'>Working With defects</option>";
 						echo "<option value='Not Working'>Not Working</option>";
 
 						echo "</select>";
 						echo "</td>";
-						echo "<td><input type='checkbox' name='returned[]' value='$itemCopyId'/></td></tr>";
+						echo "<td><input type='checkbox' name='returned[]' value='$itemCopyId' id='$itemCopyId'/><label for='$itemCopyId'><span class='ui'></span></label></td></tr>";
 						
 
 
@@ -97,7 +174,28 @@
 
 						?>
 
+
         	</div>
+
+        	<h3>Remarks</h3>
+        	
+        	<label>Comments</label>
+        	<textarea name="paragraph" class="small textarea" value="<?php echo $rcomment; ?>"></textarea> <br/>
+        	<label>User Remark</label><br/>
+        	<select class='medium select'>
+        		<option value="Borrow Allowed" selected="selected">Borrow Allowed</option>
+        		<option value="Borrow Disallowed">Borrow Disallowed</option>
+        	</select>
+        	<br/><br/>
+        	<a href="#">View Transaction History</a>
+
+
+
+        	<br/><br/>
+
+        	<input type="submit" class="button" value="     Complete Return     " name="submit" />
+        	
+        	
 			
         </div>
         </div>
---
+++ schedule_manager.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Member Management</title>
+
+
+
+<link rel="stylesheet" type="text/css" href="css/content.css" />
+<link rel="stylesheet" type="text/css" href="css/button.css" />
+<link rel="stylesheet" type="text/css" href="css/wrapper.css" />
+<link rel="stylesheet" type="text/css" href="css/form.css" />
+
+<script src="lib/jquery.min.js"></script>
+
+
+</head>
+<body>
+    <div id="wrapper">
+		<?php include "includes/header.php" ?>
+		
+		<?php include "includes/leftnav.php" ?>
+		<div id="contentwrap">
+        <div id="content">
+        
+        <div style="text-align:left;width:68%;display:inline-block;">
+        <font face="Agency FB" color="black" size="6px" ><Strong>Schedule Management Panel</Strong></font>
+        <br/><br/>
+        	
+        </div>
+
+        <div style="text-align:right;width:28%;display:inline-block;vertical-align:top;">
+			<button style="width:15em;background:#43D14C;">   <div><img src="img/icons/glyphicons_free/glyphicons/png/glyphicons-191-circle-plus.png" width="13" height="13" /><font face="Calibri" color="black" size="4"> Create new schedule </font></div></button>
+		</div>	
+
+		
+        </div>
+        </div>
+		
+       <?php include "includes/footer.php" ?>
+    </div>
+</body>
+</html>

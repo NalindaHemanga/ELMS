7ca370205f4b63745f84e36760e59bcf7e7639be
CodeImperial <hisharaperera@gmail.com>
2016-01-04 22:48:53 +0530

    add forgot password functionality(forgot_pass.php and pass_reset_complete.php files) and link that files with login page

---
+++ pass_reset_complete.php
@@ -0,0 +1,34 @@
+<?php
+
+	require("core/init.php");
+	/*save enterd new password to variables*/
+
+	$newpass = $_POST['newpass'];
+	$newpass1 = $_POST['newpass1'];
+	$post_email = $_POST['username'];
+	$code = $_GET['code'];
+
+
+	if($newpass == $newpass1){
+
+		$enc_pass = crypt($newpass);//encrypt password
+
+		/*update database password column to new password*/
+
+		$quary = "UPDATE member SET member_password='$enc_pass' WHERE member_email='$post_email'";
+		DB::getInstance()->directUpdate($quary);
+
+		/*reset the database pass_reset column to 0*/
+		$quary1 = "UPDATE member SET pass_reset='0' WHERE member_email='$post_email'";
+		DB::getInstance()->directUpdate($quary1);
+
+
+		echo "Your password has been updated.<p><a href='http://elms.16mb.com/login.php'>Click here to go back</a>";
+
+	}
+	else{
+
+		echo "Password must match <a href='forgot_pass.php?code=$code&email=$post_email'>Click here to go back</a>";
+	}
+
+?>

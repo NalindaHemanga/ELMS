5ae514fcd3684285e523e8c8d9ed4f4d437f0abf
NalindaHemanga <nalinda2hemanga@gmail.com>
2016-01-07 11:51:54 +0530

    new commmit

--- item_return_panel2_control.php
+++ item_return_panel2_control.php
@@ -4,8 +4,35 @@ require_once 'core/init.php';
 
 if(count($_POST)>0){
 
-	print_r($_POST["condition"]);
-	print_r($_POST["returned"]);
+	//print_r($_POST);
+	//print_r($_POST["condition"]);
+	//print_r($_POST["returned"]);
+
+	$tid=$_POST["tid"];
+
+	$tran=Transaction::search($tid);
+	$tran->updateComment($_POST["comment"]);
+	$tran->updateRemark($_POST["remark"]);
+
+	$condition=$_POST["condition"];
+	$returned=$_POST["returned"];
+	$i=0;
+	for($i=1;$i<count($returned);$i++) {
+		
+		
+
+			if($tran->finishReturn($returned[$i],$condition[$i])){
+				echo "ok";
+			}
+			else{
+				echo "NOt";
+			}
+
+
+
+		
+	
+	}
 
 
 }
--- time_table_slot.php
+++ time_table_slot.php
@@ -15,15 +15,15 @@ $timestamp = strtotime('2009-10-22');
 		<tr><th><center>Time</center></th><th><center><?php echo $_POST["dayy"]; ?></center></th></tr>
 	</thead>
 	<tbody>
-	<tr><td><center>8.00 a.m. - 9.00 a.m </center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
-	<tr><td><center>8.00 a.m. - 9.00 a.m </center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
-	<tr><td><center>8.00 a.m. - 9.00 a.m </center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
-	<tr><td><center>8.00 a.m. - 9.00 a.m </center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
-	<tr><td><center>8.00 a.m. - 9.00 a.m </center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
-	<tr><td><center>8.00 a.m. - 9.00 a.m </center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
-	<tr><td><center>8.00 a.m. - 9.00 a.m </center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
-	<tr><td><center>8.00 a.m. - 9.00 a.m </center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
-	<tr><td><center>8.00 a.m. - 9.00 a.m </center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>	
+	<tr><td><center>8.00 a.m. - 9.00 a.m</center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
+	<tr><td><center>9.00 a.m. - 10.00 a.m</center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
+	<tr><td><center>10.00 a.m. - 11.00 a.m</center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
+	<tr><td><center>11.00 a.m. - 12.00 a.m</center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
+	<tr><td><center>12.00 a.m. - 1.00 p.m</center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
+	<tr><td><center>1.00 p.m. - 2.00 p.m</center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
+	<tr><td><center>2.00 p.m. - 3.00 p.m</center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
+	<tr><td><center>3.00 p.m. - 4.00 p.m</center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>
+	<tr><td><center>4.00 p.m. - 5.00 p.m</center></td><td><center><input type='text' class="text large" ondrop='drop(event,"m1")' ondragover="allowDrop(event)"/></center></td></tr>	
 
 		
 

6dc4c5da7a53f1bbc7d9205ad4f25f4c96cc06cb
Bhanuka <lbhanuka@gmail.com>
2016-01-07 13:49:53 +0530

    item edit and change item copy id

---
+++ item_edit.php
@@ -0,0 +1,75 @@
+<?php
+
+    require_once 'core/init.php';
+
+
+    if(count($_POST) > 0) {
+//print_r($_POST);
+	if (isset($_POST["reference"])){
+   	$item_data = array(
+
+		"item_id"						=> 	$_POST["item_id"],
+		"item_name"	 				=> 	$_POST["item_name"],
+		"item_type"					=>	$_POST["item_type"],
+		"item_technical_details" 	=> 	$_POST["item_tec_desc"],
+		"item_description"			=>  $_POST["item_desc"],
+		//"reference" 			=>	$_POST["reference"]
+
+	);}
+	else{
+	   	$item_data = array(
+
+		"item_id"						=> 	$_POST["item_id"],
+		"item_name"	 				=> 	$_POST["item_name"],
+		"item_type"					=>	$_POST["item_type"],
+		"item_technical_details" 	=> 	$_POST["item_tec_desc"],
+		"item_description"			=>  $_POST["item_desc"],
+		//"reference" 			=>	null
+
+	);
+
+	}
+	//echo $_POST["itemTechDetEdit"];
+
+    	$temp_path=$_FILES["item_picture"]["tmp_name"];
+    	$img_type = pathinfo($_FILES["item_picture"]["name"],PATHINFO_EXTENSION);
+    	move_uploaded_file($temp_path, "img/items/" . $_POST["item_name"]."."."png");
+
+
+
+
+        //$_SESSION['form_data'] = $item_data;
+        $_SESSION['form_data'] = $item_data;
+
+        //header("Location: item_add.php",true,303);
+        //die();
+    
+
+
+
+//print_r($item_data);
+
+     $item_to_edit = Item::search(array("item_id"=>$_POST["item_id"]));
+	
+	if($item_to_edit->editItem($_SESSION['form_data'])){
+
+		$message = "You have successfully updated the item !!";
+		echo $message;
+	}
+
+	else{
+
+
+		$message = "The Item update was unsuccessful.";
+		echo $message;
+
+
+	}
+
+
+
+        unset($_SESSION["form_data"]);
+
+    }
+
+?>

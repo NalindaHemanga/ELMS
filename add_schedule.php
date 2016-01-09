3a27987a81c8e5d65d80d6c3c74e568881fa9d8e
NalindaHemanga <nalinda2hemanga@gmail.com>
2016-01-09 13:18:37 +0530

    new

--- add_schedule.php
+++ add_schedule.php
@@ -2,28 +2,40 @@
 
 require_once 'core/init.php';
 
-$data=array(
+$start_date = $_POST["sem_start_date"];
+$end_date = $_POST["sem_end_date"];
 
-	"schedule_id"=>null,
-	"semester_no"=>$_POST["semester"],
-	"academic_year"=>$_POST["academic_year"],
-	"schedule_start_date"=>$_POST["sem_start_date"],
-	"schedule_end_date"=>$_POST["sem_end_date"]
+if(strtotime($start_date)<strtotime($end_date)){
+
+	$data=array(
+
+		"schedule_id"=>null,
+		"semester_no"=>$_POST["semester"],
+		"academic_year"=>$_POST["academic_year"],
+		"schedule_start_date"=>$_POST["sem_start_date"],
+		"schedule_end_date"=>$_POST["sem_end_date"]
 
 
-	);
-$newSchedule=new Schedule();
-$newSchedule->create($data);
-if($newSchedule->add()){
+		);
+	$newSchedule=new Schedule();
+	$newSchedule->create($data);
+		if($newSchedule->add()){
 
-	echo "You have successfully added a schedule !";
+			echo "You have successfully added a schedule !";
 
+
+		}
+		else{
+
+			echo "Schedule creation was unsuccessful !";
+
+
+		}
 
 }
 else{
 
-	echo "Schedule creation was unsuccessful !";
-
+	echo "Schedule Start date must be less than Schedule end data !!";
 
 }
 
